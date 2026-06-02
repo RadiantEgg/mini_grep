ab86e87dfa10d6020148689f5bdf56eaea100479
radiantegg <2173590735@qq.com>
2026-06-02 15:28:52 +0800

    add file.h and match.h

---
+++ include/file.h
@@ -0,0 +1,8 @@
+#ifndef FILE_H
+#define FILE_H
+
+#include "config.h"
+
+void process_file(Config *cfg, char *filename);
+
+#endif
---
+++ include/match.h
@@ -0,0 +1,9 @@
+#ifndef MATCH_H
+#define MATCH_H
+
+#include "config.h"
+
+int match_line(Config *cfg, char *line);
+
+#endif
+
